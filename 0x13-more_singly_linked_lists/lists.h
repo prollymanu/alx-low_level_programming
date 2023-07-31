48dd9e003adf1429e804be2b97dc6accd67ff904
prollymanu <pemmanuel218@gmail.com>
2023-07-31 14:02:09 -0700

    0x13.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint(const listint_t *k);
 size_t listint_len(const listint_t *k);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t *k);
+size_t free_listint_safe(listint_t **k);
 listint_t *find_listint_loop(listint_t *head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
